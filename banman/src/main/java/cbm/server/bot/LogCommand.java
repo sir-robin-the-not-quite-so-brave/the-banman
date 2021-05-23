483702b4374cf5b9bd5eb75de82c6585fce388e7
Sir Robin The Not-Quite-So-Brave <sir_robin_not_so_brave@protonmail.com>
2021-05-23 12:20:19 +0300

    Show ban duration

--- banman/src/main/java/cbm/server/bot/LogCommand.java
+++ banman/src/main/java/cbm/server/bot/LogCommand.java
@@ -3,9 +3,11 @@ package cbm.server.bot;
 import cbm.server.Bot;
 import cbm.server.SteamID;
 import cbm.server.db.BansDatabase;
+import cbm.server.model.Ban;
 import org.jetbrains.annotations.NotNull;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.util.List;
 
 public class LogCommand implements BotCommand {
@@ -13,6 +15,7 @@ public class LogCommand implements BotCommand {
     private static final String[] DESCRIPTION = new String[]{
             "*id-or-url* - Shows player's ban history."
     };
+    private static final int SECONDS_PER_DAY = 24 * 3600;
 
     private final BansDatabase bansDatabase;
 
@@ -45,8 +48,8 @@ public class LogCommand implements BotCommand {
 
         final StringBuilder sb = new StringBuilder("**Ban history** ");
         sb.append(steamID.profileUrl()).append(":\n```diff");
-        for (var ban : banHistory) {
-            switch (ban.getAction()) {
+        for (var banLogEntry : banHistory) {
+            switch (banLogEntry.getAction()) {
                 case "add":
                     sb.append("\n+ ");
                     break;
@@ -55,14 +58,40 @@ public class LogCommand implements BotCommand {
                     sb.append("\n- ");
                     break;
             }
-            sb.append(ban.getDetectedAt()).append(": \"")
-              .append(ban.getBan().getPlayerName()).append("\" banned from ")
-              .append(ban.getBan().getEnactedTime())
-              .append(" until ").append(ban.getBan().getBannedUntil())
-              .append(" for \"").append(ban.getBan().getReason()).append("\"");
+            final Ban ban = banLogEntry.getBan();
+            sb.append(banLogEntry.getDetectedAt()).append(": \"")
+              .append(ban.getPlayerName()).append("\" banned from ")
+              .append(ban.getEnactedTime())
+              .append(" until ").append(ban.getBannedUntil())
+              .append(" (duration ");
+            append(sb, ban.getDuration());
+            sb.append(") for \"").append(ban.getReason()).append("\"");
         }
         sb.append("```");
 
         return sb.toString();
+    }
+
+    private static void append(StringBuilder sb, Duration duration) {
+        final long seconds = duration.toSeconds();
+        final long days = seconds / SECONDS_PER_DAY;
+        if (days == 0) {
+            sb.append(duration);
+            return;
+        }
+
+        final long weeksPart = days / 7;
+        final long daysPart = days % 7;
+        final long secondsPart = seconds % SECONDS_PER_DAY;
+        final Duration subDayDuration = Duration.ofSeconds(secondsPart);
+        sb.append("P");
+        if (weeksPart != 0)
+            sb.append(weeksPart).append("W");
+
+        if (daysPart != 0)
+            sb.append(daysPart).append("D");
+
+        final String s = subDayDuration.toString();
+        sb.append(s, 1, s.length());
     }
 }
