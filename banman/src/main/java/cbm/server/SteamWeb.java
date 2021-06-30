d4e9817d1e29d8dbcb1d688c98fb9758469313b3
Sir Robin The Not-Quite-So-Brave <sir_robin_not_so_brave@protonmail.com>
2021-06-30 11:23:55 +0300

    Cache the calls to Steam and steamid.io

--- banman/src/main/java/cbm/server/SteamWeb.java
+++ banman/src/main/java/cbm/server/SteamWeb.java
@@ -1,5 +1,9 @@
 package cbm.server;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.LoadingCache;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
@@ -8,14 +12,29 @@ import reactor.core.publisher.Mono;
 
 import java.io.IOException;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import static cbm.server.Utils.asyncOne;
 
-// TODO Cache the external calls
 public class SteamWeb {
 
-    public static @NotNull Profile downloadProfile(@NotNull String profileUrl) throws IOException {
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    private static final LoadingCache<String, Profile> PROFILES_CACHE =
+            Caffeine.newBuilder()
+                    .maximumSize(1_000)
+                    .expireAfterWrite(10, TimeUnit.MINUTES)
+                    .build(SteamWeb::downloadProfile);
+
+    private static final LoadingCache<String, SteamID> STEAM_ID_CACHE =
+            Caffeine.newBuilder()
+                    .maximumSize(1_000)
+                    .expireAfterWrite(10, TimeUnit.MINUTES)
+                    .build(s -> resolveId(s).flatMap(SteamID::steamID).orElse(null));
+
+    private static @NotNull Profile downloadProfile(@NotNull String profileUrl) throws IOException {
+        LOGGER.debug("Downloading profile: {}", profileUrl);
         final var doc = Jsoup.connect(profileUrl).get();
         final var name =
                 doc.select(".actual_persona_name").stream()
@@ -47,7 +66,8 @@ public class SteamWeb {
         };
     }
 
-    public static Optional<String> resolveId(@NotNull String s) throws IOException {
+    private static Optional<String> resolveId(@NotNull String s) throws IOException {
+        LOGGER.debug("Resolving: {}", s);
         final var doc = Jsoup.connect("https://steamid.io/lookup")
                              .data("input", s)
                              .post();
@@ -68,13 +88,13 @@ public class SteamWeb {
     }
 
     public static Mono<Profile> playerProfile(@NotNull String profileUrl) {
-        return asyncOne(() -> downloadProfile(profileUrl));
+        return asyncOne(() -> PROFILES_CACHE.get(profileUrl));
     }
 
     public static Mono<SteamID> resolveSteamID(@NotNull String s) {
-        return asyncOne(() -> resolveId(s).flatMap(SteamID::steamID)
-                                          .orElseThrow(() -> new IllegalArgumentException("Cannot resolve steam ID: "
-                                                                                                  + s)));
+        return asyncOne(() -> Optional.ofNullable(STEAM_ID_CACHE.get(s))
+                                      .orElseThrow(() -> new IllegalArgumentException("Cannot resolve steam ID: "
+                                                                                              + s)));
     }
 
     @SuppressWarnings("unused")
